ca055eda854d482d03a96696b53244333259aa5d
dmitriy <dvasilenko1999@gmail.com>
2026-04-15 16:13:31 +0300

    client (update): use get comments hooks - update query key

--- client/src/entities/comments/model/hooks/use-get-recipe-comments.ts
+++ client/src/entities/comments/model/hooks/use-get-recipe-comments.ts
@@ -2,9 +2,11 @@ import { useQuery, keepPreviousData } from '@tanstack/react-query';
 import { getRecipeComments, type GetRecipeCommentsArgs } from '../../api';
 
 export const useGetRecipeComments = (args: GetRecipeCommentsArgs) => {
+  const { recipeId, ...restArgs } = args;
+
   return useQuery({
     queryFn: () => getRecipeComments(args),
-    queryKey: ['comments', { ...args }],
+    queryKey: ['comments', recipeId, { ...restArgs }],
     placeholderData: keepPreviousData,
   });
 };
