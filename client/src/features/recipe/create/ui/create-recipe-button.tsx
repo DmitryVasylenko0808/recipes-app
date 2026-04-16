6891b0c3121755233eb5a275e853c0c1dc164024
dmitriy <dvasilenko1999@gmail.com>
2026-04-16 15:52:57 +0300

    client (update): empty state for author-recipes & update create recipe button

--- client/src/features/recipe/create/ui/create-recipe-button.tsx
+++ client/src/features/recipe/create/ui/create-recipe-button.tsx
@@ -1,14 +1,16 @@
-import { Button, pathKeys } from '@/shared';
+import { Button, pathKeys, type BaseButtonProps } from '@/shared';
 import { Plus } from 'lucide-react';
 import { useNavigate } from 'react-router';
 
-export const CreateRecipeButton = () => {
+type CreateRecipeButtonProps = Partial<Pick<BaseButtonProps, 'variant'>>;
+
+export const CreateRecipeButton = ({ variant = 'secondary' }: CreateRecipeButtonProps) => {
   const navigate = useNavigate();
 
   const handleClick = () => navigate(pathKeys.recipes.create);
 
   return (
-    <Button as="button" variant="secondary" icon={Plus} onClick={handleClick}>
+    <Button as="button" variant={variant} icon={Plus} onClick={handleClick}>
       Create Recipe
     </Button>
   );
--- client/src/pages/author/ui/author-recipes.tsx
+++ client/src/pages/author/ui/author-recipes.tsx
@@ -5,10 +5,11 @@ import {
   RecipeCard,
 } from '@/entities/recipes';
 import { usePagination, Pagination } from '@/features/pagination';
-import { Typograpghy } from '@/shared';
+import { EmptyState, Typograpghy, useAuth } from '@/shared';
 import { useParams } from 'react-router';
 import { INITIAL_LIMIT } from '../constants';
 import { ToggleFavoriteRecipeButton } from '@/features/favorites/toggle';
+import { CreateRecipeButton } from '@/features/recipe/create';
 
 export const AuthorRecipes = () => {
   const { id } = useParams();
@@ -20,27 +21,42 @@ export const AuthorRecipes = () => {
     page,
     limit,
   });
+  const { currentUser } = useAuth();
+
+  const isOwnProfile = id === currentUser?.id;
 
   return (
     <div>
-      <Typograpghy tagVariant="h3" className="mb-6">
-        Author recipes ({data?.totalCount})
-      </Typograpghy>
       {isPending ? (
         <RecipesGridSkeleton countItems={16} cols={4} />
-      ) : (
-        <RecipesGrid
-          isFetching={isFetching}
-          recipes={data?.data}
-          cols={4}
-          renderItems={(r) => (
-            <RecipeCard
-              recipe={r}
-              key={r.id}
-              actionsSlot={<ToggleFavoriteRecipeButton recipe={r} />}
-            />
-          )}
+      ) : data?.totalCount === 0 ? (
+        <EmptyState
+          title="No recipes yet"
+          description={
+            isOwnProfile
+              ? 'You haven’t created any recipes yet. Start sharing your cooking ideas.'
+              : 'This user hasn’t shared any recipes yet.'
+          }
+          action={isOwnProfile && <CreateRecipeButton variant="primary" />}
         />
+      ) : (
+        <div>
+          <Typograpghy tagVariant="h3" className="mb-6">
+            Author recipes ({data?.totalCount})
+          </Typograpghy>
+          <RecipesGrid
+            isFetching={isFetching}
+            recipes={data?.data}
+            cols={4}
+            renderItems={(r) => (
+              <RecipeCard
+                recipe={r}
+                key={r.id}
+                actionsSlot={<ToggleFavoriteRecipeButton recipe={r} />}
+              />
+            )}
+          />
+        </div>
       )}
       <Pagination totalPages={data?.totalPages} currentPage={page} onPageChange={onPageChange} />
     </div>
