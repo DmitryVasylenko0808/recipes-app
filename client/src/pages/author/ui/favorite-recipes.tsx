9d409e942659fed4e80a0c00b500d6a2af48a7d5
dmitriy <dvasilenko1999@gmail.com>
2026-04-16 15:53:14 +0300

    client (update): empty state for favorite recipes

--- client/src/pages/author/ui/favorite-recipes.tsx
+++ client/src/pages/author/ui/favorite-recipes.tsx
@@ -1,7 +1,7 @@
 import { useGetFavoriteRecipes } from '@/entities/favorites';
 import { RecipesGridSkeleton, RecipesGrid, RecipeCard } from '@/entities/recipes';
 import { usePagination, Pagination } from '@/features/pagination';
-import { Typograpghy } from '@/shared';
+import { EmptyState, Typograpghy } from '@/shared';
 import { INITIAL_LIMIT } from '../constants';
 import { ToggleFavoriteRecipeButton } from '@/features/favorites/toggle';
 
@@ -9,6 +9,7 @@ export const FavoriteRecipes = () => {
   const { page, limit, onPageChange } = usePagination({
     initialLimit: INITIAL_LIMIT,
   });
+
   const { data, isPending, isFetching } = useGetFavoriteRecipes({
     page,
     limit,
@@ -16,24 +17,31 @@ export const FavoriteRecipes = () => {
 
   return (
     <div>
-      <Typograpghy tagVariant="h3" className="mb-6">
-        Favorite recipes ({data?.totalCount})
-      </Typograpghy>
       {isPending ? (
         <RecipesGridSkeleton countItems={16} cols={4} />
-      ) : (
-        <RecipesGrid
-          isFetching={isFetching}
-          recipes={data?.data.map((fr) => fr.recipe)}
-          cols={4}
-          renderItems={(r) => (
-            <RecipeCard
-              recipe={r}
-              key={r.id}
-              actionsSlot={<ToggleFavoriteRecipeButton recipe={r} />}
-            />
-          )}
+      ) : data?.totalCount === 0 ? (
+        <EmptyState
+          title="No favorite recipes yet"
+          description="Start adding recipes to favorites to see them here!"
         />
+      ) : (
+        <div>
+          <Typograpghy tagVariant="h3" className="mb-6">
+            Favorite recipes ({data?.totalCount})
+          </Typograpghy>
+          <RecipesGrid
+            isFetching={isFetching}
+            recipes={data?.data.map((fr) => fr.recipe)}
+            cols={4}
+            renderItems={(r) => (
+              <RecipeCard
+                recipe={r}
+                key={r.id}
+                actionsSlot={<ToggleFavoriteRecipeButton recipe={r} />}
+              />
+            )}
+          />
+        </div>
       )}
       <Pagination totalPages={data?.totalPages} currentPage={page} onPageChange={onPageChange} />
     </div>
