058d7aaab6933dc58c59606a464bd3916bef2079
dmitriy <dvasilenko1999@gmail.com>
2026-04-23 17:59:42 +0300

    client (update): optimistic update after rate recipe

--- client/src/features/recipe/rate/model/hooks/use-rate-recipe.ts
+++ client/src/features/recipe/rate/model/hooks/use-rate-recipe.ts
@@ -6,8 +6,24 @@ export const useRateRecipe = () => {
 
   return useMutation({
     mutationFn: putRateRecipe,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['recipes'] });
+    onMutate: async (variables) => {
+      await queryClient.cancelQueries({ queryKey: ['recipes', variables.id] });
+
+      const prev = queryClient.getQueryData(['recipes', variables.id]);
+
+      queryClient.setQueryData(
+        ['recipes', variables.id],
+        (old: any) =>
+          old && { ...old, ratingsCount: old.ratingsCount + 1, userRating: variables.value }
+      );
+
+      return { prev };
+    },
+    onError: (_, variables, onMutateResult) => {
+      queryClient.setQueryData(['recipes', variables.id], onMutateResult?.prev);
+    },
+    onSettled: (data) => {
+      queryClient.invalidateQueries({ queryKey: ['recipes', data?.recipeId] });
     },
   });
 };
