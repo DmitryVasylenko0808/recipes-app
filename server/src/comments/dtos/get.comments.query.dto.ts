28e61e2232ba73f795e299d27ef4db13e782a995
dmitriy <dvasilenko1999@gmail.com>
2026-05-01 16:02:48 +0300

    server (update): update doc (swagger) of get comments query dto

--- server/src/comments/dtos/get.comments.query.dto.ts
+++ server/src/comments/dtos/get.comments.query.dto.ts
@@ -2,8 +2,15 @@ import { Expose } from 'class-transformer';
 import { IsEnum, IsOptional } from 'class-validator';
 import { PaginationQueryDto } from 'src/recipes/dtos';
 import { SortCommentsPreset } from '../types';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class GetCommentsQueryDto extends PaginationQueryDto {
+  @ApiPropertyOptional({
+    enum: SortCommentsPreset,
+    description: 'Sort comments by newest (by createdAt:desc) or popular (by likesCount:desc)',
+    default: 'newest',
+    example: 'newest',
+  })
   @Expose({ name: 'sort' })
   @IsOptional()
   @IsEnum(SortCommentsPreset, { message: 'Invalid sort' })
